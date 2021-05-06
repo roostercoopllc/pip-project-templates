919a634757f35decf6711fa3405441a2448411a7
Derek <derek@roostercoop.ninja>
2021-05-06 13:13:54 -0400

    initial commit

---
+++ {{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='{{cookiecutter.project_name}}',
+    version='0.1.0',    
+    description='{{cookiecutter.project_description}}',
+    url='{{cookiecutter.project_github}}',
+    author='{{cookiecutter.author}}',
+    author_email='{{cookiecutter.author_email}}',
+    license='{{cookiecutter.license}}',
+    packages=['{{cookiecutter.project_name}}'],
+    install_requires=['',
+                      '',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Starting',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: {{cookiecutter.license}}',  
+        'Operating System :: POSIX :: {{cookiecutter.operating_system}}',        
+        'Programming Language :: Python :: {{cookiecutter.python_version}}',
+    ],
+)
