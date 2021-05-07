6db8d6c1b532be4ef671ce9a1038ba7a949e3718
Derek <derek@roostercoop.ninja>
2021-05-06 21:34:39 -0400

    restructured because previous structure rubbish

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='{{cookiecutter.project_name}}',
     version='0.1.0',    
+    # scripts=['{{cookiecutter.project_name}}'],
     description='{{cookiecutter.project_description}}',
     url='{{cookiecutter.project_github}}',
     author='{{cookiecutter.author}}',
